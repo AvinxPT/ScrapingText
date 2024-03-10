b5543e25839d03c4103e3cde662a0b1b0b41de25
AvinxPT <45512358+AvinxPT@users.noreply.github.com>
2024-03-10 17:44:06 +0000

    Scraping temperature after login

--- main.py
+++ main.py
@@ -28,13 +28,11 @@ def clean_text(text):
 def main():
     driver = get_driver()
     driver.find_element(by="id", value="id_username").send_keys("automated")
-    time.sleep(2)
     driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
-    time.sleep(2)
-    print(driver.current_url)
-    time.sleep(2)
     driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-    print(driver.current_url)   
+    time.sleep(2)
+    element = driver.find_element(by="id", value="displaytimer")
+    return clean_text(element.text)
 
 
 print(main())
